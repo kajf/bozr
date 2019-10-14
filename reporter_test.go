b48f98516e30d86e42c7e145fe2116ed13f53940
Kajf <letsiaha@gmail.com>
2019-10-14 09:18:49 +0300

    #88 Unclear failure message in case of invalid 'remember' 'bodyPath' - test

--- reporter_test.go
+++ reporter_test.go
@@ -1,15 +1,59 @@
 package main
 
 import (
+	"errors"
+	"github.com/fatih/color"
+	"strings"
+	"sync"
 	"testing"
 )
 
-func TestJUnitReporterEmptyResults(t *testing.T) {
+func TestConsoleReporterReport_ErrorAfterPassedExp_Reported(t *testing.T) {
 	// given
+	reportedError := "test err"
+	results := []TestResult{
+		{
+			Traces: []*CallTrace{
+				{
+					ExpDesc:    map[string]bool{"Status code is 200": false},
+					ErrorCause: errors.New(reportedError),
+				},
+			},
+		}, // error without expectation description
+	}
+
+	writer := MockWriter{
+		expectedWriting: reportedError,
+	}
+
+	reporter := &ConsoleReporter{ExitCode: 0, Writer: &writer, ioMutex: &sync.Mutex{}, LogHTTP: false}
+
+	color.Output = &writer // prevent stdout and invalid test result parsing in IDE (reacts on words 'FAILED')
 
 	// when
-	NewJUnitReporter("").Report([]TestResult{})
+	reporter.Report(results)
 
 	// then
-	// no nil pointer panic
+	if !writer.passed() {
+		t.Errorf("Expected writing %s was not met in %s", writer.expectedWriting, writer.actualWriting)
+	}
+}
+
+type MockWriter struct {
+	expectedWriting string
+	actualWriting   string
+}
+
+func (mw *MockWriter) Write(p []byte) (n int, err error) {
+
+	in := string(p)
+	mw.actualWriting += in
+
+	//os.Stdout.Write(p)
+
+	return len(p), nil
+}
+
+func (mw *MockWriter) passed() bool {
+	return strings.Contains(mw.actualWriting, mw.expectedWriting)
 }
